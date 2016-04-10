f9071c095a7b6d5148ad9f5e0a6067cecc6c7e4c
Zach Ohara <zjohara@gmail.com>
2016-04-10 15:08:05 -0700

    Rename 'CardColumnScene' to 'WorkspaceScene'

--- src/io/github/zachohara/percussionpacker/window/WorkspaceScene.java
+++ src/io/github/zachohara/percussionpacker/window/WorkspaceScene.java
@@ -18,27 +18,34 @@ package io.github.zachohara.percussionpacker.window;
 
 import io.github.zachohara.percussionpacker.card.Card;
 import io.github.zachohara.percussionpacker.card.NameField;
+import io.github.zachohara.percussionpacker.event.RegionResizeListener;
 import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
-public class CardColumnScene extends Scene {
+public class WorkspaceScene extends Scene {
 	
-	public static final int DEFAULT_WIDTH = 500;
-	public static final int DEFAULT_HEIGHT = 500;
+	private ColumnTitle[] columnTitles;
 	
 	private Pane primaryPane;
+	
+	private RegionResizeListener resizeListener;
 
-	public CardColumnScene() {
-		super(new Pane());
+	public WorkspaceScene() {
+		super(new GridPane());
 		this.primaryPane = (Pane) this.getRoot();
-		this.primaryPane.setPrefSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		this.primaryPane.setPrefSize(Window.DEFAULT_WIDTH, Window.DEFAULT_HEIGHT);
 		this.addEventHandler(MouseEvent.ANY, new MouseHandler());
+		this.resizeListener = new RegionResizeListener(this);
+		
+		this.initializeColumnTitles();
 	}
 	
+	//Old methods: only uncomment if absolutely necessary (they shouldn't be)
 	public Pane getPrimaryPane() {
 		return this.primaryPane;
 	}
@@ -51,17 +58,32 @@ public class CardColumnScene extends Scene {
 		return this.getPrimaryPane().getChildren().addAll(c);
 	}
 	
+	private void initializeColumnTitles() {
+		// Create the titles
+		this.columnTitles = new ColumnTitle[4];
+		this.columnTitles[0] = new ColumnTitle("Song List");
+		this.columnTitles[1] = new ColumnTitle("Complete\nEquipment List");
+		this.columnTitles[2] = new ColumnTitle("Packing List");
+		this.columnTitles[3] = new ColumnTitle("Mallet List");
+		
+		for (int i = 0; i < 4; i++) {
+			GridPane.setColumnIndex(this.columnTitles[i], i);
+			this.resizeListener.addHandler(this.columnTitles[i]);
+		}
+		this.primaryPane.getChildren().addAll(this.columnTitles);
+	}
+	
 	private class MouseHandler implements EventHandler<MouseEvent> {
 
 		@Override
 		public void handle(MouseEvent event) {
 			EventType<? extends MouseEvent> type = event.getEventType();
 			if (type == MouseEvent.MOUSE_CLICKED) {
-				Node focusedObject = CardColumnScene.this.getFocusOwner();
+				Node focusedObject = WorkspaceScene.this.getFocusOwner();
 				if (focusedObject instanceof NameField) {
 					Card activeCard = ((NameField) focusedObject).getCard();
 					if (!activeCard.containsScenePoint(event.getSceneX(), event.getSceneY())) {
-						CardColumnScene.this.primaryPane.requestFocus();
+						WorkspaceScene.this.primaryPane.requestFocus();
 					}
 				}
 			}
