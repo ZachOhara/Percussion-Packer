0992fa431f949c75a6b1f6455c6785d0d8302c5e
Zach Ohara <zjohara@gmail.com>
2016-04-10 21:19:53 -0700

    Late happy new year
    One of the few downsides of using code templates...

--- src/io/github/zachohara/percussionpacker/event/ResizeHandler.java
+++ src/io/github/zachohara/percussionpacker/event/ResizeHandler.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2015 Zach Ohara
+/* Copyright (C) 2016 Zach Ohara
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
