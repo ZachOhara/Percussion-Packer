987bc18185db020a799800a6f7703d9c2f31fd56
Zach Ohara <zjohara@gmail.com>
2016-04-13 10:50:41 -0700

    Create ghost card class

---
+++ src/io/github/zachohara/percussionpacker/card/GhostCard.java
@@ -0,0 +1,12 @@
+package io.github.zachohara.percussionpacker.card;
+
+import javafx.scene.layout.Pane;
+
+public class GhostCard extends Pane {
+	
+	public GhostCard() {
+		super();
+		this.setPrefHeight(Card.HEIGHT);
+	}
+
+}
