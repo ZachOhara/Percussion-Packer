635a2071410307c17991ef2bf04477b524dcd07d
Zach Ohara <zjohara@gmail.com>
2016-04-19 10:21:06 -0700

    Add a simple 'ruler window' test class

---
+++ src/io/github/zachohara/percussionpacker/test/RulerWindow.java
@@ -0,0 +1,37 @@
+package io.github.zachohara.percussionpacker.test;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class RulerWindow extends Application implements ChangeListener<Number> {
+
+	private Pane pane;
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		arg0 = new Stage(StageStyle.UTILITY);
+		pane = new Pane();
+		pane.setStyle("-fx-background-color: black");
+		pane.widthProperty().addListener(this);
+		pane.heightProperty().addListener(this);
+		Scene s = new Scene(pane);
+		arg0.setScene(s);
+		
+		arg0.show();
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+		System.out.println("Ayy " + this.pane.getWidth() + " " + this.pane.getHeight());
+	}
+
+}
