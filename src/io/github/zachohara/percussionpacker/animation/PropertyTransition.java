7ef2077193ae8a36d4dbeae282b7db00cf928abe
Zach Ohara <zjohara@gmail.com>
2016-05-08 01:06:01 -0700

    Add a new JavaFX-based animation system
    This new system uses the built-in JavaFX animation system, so it should
    run without all the lag caused by multi-threading workarounds.

---
+++ src/io/github/zachohara/percussionpacker/animation/PropertyTransition.java
@@ -0,0 +1,37 @@
+/* Copyright (C) 2016 Zach Ohara
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.zachohara.percussionpacker.animation;
+
+import javafx.animation.Transition;
+import javafx.beans.property.DoubleProperty;
+
+public class PropertyTransition extends Transition {
+	
+	private DoubleProperty property;
+	private InterpolatedQuantity interpolator;
+	
+	public PropertyTransition(DoubleProperty property, double difference) {
+		this.property = property;
+		this.interpolator = new InterpolatedQuantity(property.get(), difference);
+	}
+	
+	@Override
+	protected void interpolate(double fraction) {
+		this.property.set(this.interpolator.getInterpolatedValue(fraction));
+	}
+	
+}
---
+++ src/io/github/zachohara/percussionpacker/animation/WidthTransition.java
@@ -0,0 +1,35 @@
+/* Copyright (C) 2016 Zach Ohara
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.zachohara.percussionpacker.animation;
+
+import javafx.scene.layout.Region;
+
+public class WidthTransition extends ResizingTransition {
+	
+	private Region resizingRegion;
+	
+	public WidthTransition(Region resizing, double newWidth) {
+		super(resizing.getWidth(), newWidth);
+		this.resizingRegion = resizing;
+	}
+
+	@Override
+	protected void setCurrentDim(double currentValue) {
+		this.resizingRegion.setPrefWidth(currentValue);
+	}
+	
+}
